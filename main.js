8e04687b1aad4b18987bbd57c0f7e24d3a940d29
khang-kunz <khangdev171998@gmail.com>
2023-05-06 13:00:23 +0700

    first upload

---
+++ main.js
@@ -0,0 +1,131 @@
+let carts = document.querySelectorAll('.add-cart');
+
+let products = [
+    {
+        name: 'Bàn phím razer',
+        tag: 'img1',
+        price: 20,
+        inCart:0
+    },
+    {
+        name: 'Màn hình acer',
+        tag: 'img2',
+        price: 150,
+        inCart:0
+    },
+    {
+        name: 'Keyboard logitech pro x',
+        tag: 'img3',
+        price: 15,
+        inCart:0
+    },
+    {
+        name: 'Macbook m1',
+        tag: 'img4',
+        price: 20,
+        inCart:0
+    },
+    {
+        name: 'tai nghe dare u',
+        tag: 'img5',
+        price: 15,
+        inCart:0
+    },
+    
+    {
+        name: 'Bàn phím razer',
+        tag: 'img6',
+        price: 15,
+        inCart:0
+    },
+    
+];
+for (let i=0; i < carts.length; i++) {
+   carts[i].addEventListener('click', () => {
+        cartNumbers(products[i]);
+        totalCost(products[i])
+   })
+}
+function onLoadCartNumbers() {
+   let productNumbers = localStorage.getItem('cartNumbers');
+
+   if (productNumbers) {
+    document.querySelector('.cart span ').textContent = productNumbers;
+   }
+}
+function cartNumbers(product) {
+    
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    productNumbers = parseInt(productNumbers);
+
+    if( productNumbers ) {
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+        document.querySelectorAll('.cart span').textContent = productNumbers + 1;
+    } else {
+        localStorage.setItem('cartNumbers', 1);
+        document.querySelectorAll('.cart span').textContent = 
+        1;
+    }
+    SetItems(product);
+}
+function SetItems(product) {
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+    
+    if(cartItems != null) {
+
+        if(cartItems[product.tag] == undefined){
+            cartItems = {
+                ...cartItems,
+                [product.tag]: product
+            }
+        }
+        cartItems[product.tag].inCart += 1;
+
+    } else {
+        product.inCart = 1;
+        cartItems = {
+        [product.tag]: product
+        }
+    } 
+   
+    localStorage.setItem("productsInCart", JSON.stringify
+    (cartItems));
+}
+function totalCost(product) {
+    //console.log("The product price is", product.price);
+    let cartCost= localStorage.getItem('totalCost');
+
+    console.log("My cartCost is", cartCost);
+    console.log(typeof cartCost);
+
+    if(cartCost != null) {
+        cartCost = parseInt(cartCost);
+        localStorage.setItem("totalCost", cartCost + 
+        product.price);
+    } else {
+        localStorage.setItem("totalCost",product.price)
+    }
+}
+function displayCart() {
+    let cartItems = localStorage.getItem("productsInCart");
+    cartItems = JSON.parse(cartItems);
+    let productContainer = document.querySelector
+    (".products");
+    console.log(products);
+    if(productContainer ) {
+       productContainer.innerHTML = '';
+       Object.values(products).map(item => {
+        productContainer.innerHTML += `
+       <div class="product">
+       <ion-icon name="close-circle"></ion-icon>
+        <img src="./images/${item.tag}.jpg">
+        <span>${item.name}</span>
+        </div>
+       `
+    });
+    }
+}
+onLoadCartNumbers();
+displayCart();
